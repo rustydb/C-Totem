f5c956ded0a709113ca6bdad36d2e906eba28645
Russell Bunch <rusty@4lambda.io>
2017-06-17 03:42:46 -0500

    Knob works; click for setting menu; spin for variation

---
+++ control.ino
@@ -0,0 +1,187 @@
+/*
+    control.ino
+
+    Input oriented functions for a 'strip'.
+*/
+static char const* settingModes[] = {"Animations", "Speed", "Brightness"};
+
+// Update the mode.
+void updateMode() {
+    // Set this to number of cases in applyKnob().
+    if (settingMode < 2) {
+        settingMode++;
+    } else {
+        settingMode = 0;
+    }
+    currentEncoderValue = previousEncoderValue = 0;
+    printSetting();
+
+}
+
+// Update the animation setting value.
+void updateValue() {
+    if (currentEncoderValue > previousEncoderValue) {
+        if (settingValue >= 254) {
+            settingValue = 10;
+        } else {
+            settingValue++;
+        }
+    } else if (currentEncoderValue < previousEncoderValue) {
+        if (settingValue > 11) {
+            settingValue = 0;
+        } else {
+            settingValue--;
+        }
+    }
+}
+
+// Knob interrupt events.
+void readKnob() {
+    currentEncoderValue     += knob->getValue();
+    ClickEncoder::Button b   = knob->getButton();
+    if (b != ClickEncoder::Open) {
+        switch (b) {
+            case ClickEncoder::Clicked:
+                onboardLED(false);
+                updateMode();
+                break;
+            case ClickEncoder::Released:
+                onboardLED(true);
+                break;
+            default:
+                ;
+        }
+    }
+    if (b == ClickEncoder::Open) {
+        updateValue();
+        if (currentEncoderValue != previousEncoderValue) {
+            applyKnob();
+        }
+    }
+}
+
+// Applies the knob's currentEncoderValue to the strip.
+void applyKnob() {
+    /* Legend:
+        0 - Change Pattern
+        1 - Set Speed
+        2 - Set Brightness
+    */
+    switch (settingMode) {
+        case 0:
+            changeAnimation();
+            break;
+        case 1:
+            setSpeed();
+            break;
+        case 2:
+            //Changes the "brightness" value of the whole strip. Not exactly what you'd think...
+            setBrightness();
+            break;
+        default:
+            ;
+    }
+    previousEncoderValue = currentEncoderValue;
+}
+
+// Changes the animation of the strip.
+void changeAnimation() {
+    /* Legend:
+        0 - shift
+        1 - shift
+        2 - shift
+        3 - shift
+        4 - shift
+        5 - cycleSolid
+    */
+  switch (settingValue) {
+    case 0:
+//      fill_solid(&(infinity[0]), 60, CRGB::Black);
+//      meteorChaser(15, 12, 160, false);
+      break;
+    case 1:
+//      if (settingMode == 0) {
+//        fill_solid(&(infinity[0]), 60, CRGB::Black);
+//      }
+//      fourPoints(7, 21, 35, 49);
+      break;
+    case 2:
+//      colorCounter = random8();
+//      fillSolid(colorCounter);
+//      colorCounter = random8();
+//      meteorChaser(20, 14, 160, true);
+      break;
+    case 3:
+//      fillSolid(colorCounter);
+//      meteorChaser(30, 26, 160, true);
+      break;
+    case 4:
+//      wipeInfinity(25);
+      break;
+    case 5:
+//      fillSolid(colorCounter);
+      break;
+    default:
+      break;
+  }
+  previousEncoderValue = currentEncoderValue;
+}
+
+// Sets the speed of the pattern from knob values.
+void setSpeed() {
+    // Set a minimum and maximum.
+  if (currentEncoderValue > previousEncoderValue) {
+    if (speed > minimumSpeed) {
+        Serial.print("Slow down to ");
+        speed -= 2;
+        Serial.println(speed);
+    } else {
+        Serial.print("At minimum speed: ");
+        Serial.println(minimumSpeed);
+    }
+  } else if (currentEncoderValue < previousEncoderValue) {
+    if (speed < maximumSpeed) {
+        Serial.print("Speed up to ");
+        speed += 2;
+        Serial.println(speed);
+    } else {
+        Serial.print("At maximum speed: ");
+        Serial.println(maximumSpeed);
+    }
+  }
+}
+
+// Brightens or dims the strip from knob values.
+void setBrightness() {
+    if (currentEncoderValue > previousEncoderValue) {
+        if (brightness > 250) {
+            Serial.print("At maximum brightness: ");
+            Serial.println(maximumBrightness);
+        } else {
+            Serial.print("Brighten to ");
+            brightness += 5;
+            Serial.println(brightness);
+        }
+    } else if (currentEncoderValue < previousEncoderValue) {
+        if (brightness <= 0) {
+            Serial.print("At minimum brightness: ");
+            Serial.println(minimumBrightness);
+        } else {
+            Serial.print("Dim to ");
+            brightness -= 5;
+            Serial.println(brightness);
+        }
+    }
+    strip.setBrightness(brightness);
+}
+
+// Interrupt for encoder knob.
+void timerISR() {
+    knob->service();
+}
+
+// Print the setting using the global list.
+void printSetting() {
+    Serial.print("Configuring: ");
+    Serial.println(settingModes[settingMode]);
+}
--- totem.ino
+++ totem.ino
@@ -1,130 +1,49 @@
 #include <Adafruit_NeoPixel.h>
-#ifdef __AVR__
-#include <avr/power.h>
-#endif
+#include "TimerOne.h"
+#include "ClickEncoder.h"
 
-#define STRIP_NEO_PIN 6
-#define ONBOARD_LED_PIN 7
-#define ONBOARD_NEO_PIN 8
+// Used PINs.
+#define PIN_NEO_STRIP 6
+#define PIN_ONBD_LED 7
+#define PIN_ONBD_NEO 8
+#define PIN_KNOB_A 9
+#define PIN_KNOB_B 10
+#define PIN_KNOB_SWITCH 12
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(61, STRIP_NEO_PIN, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel onboard = Adafruit_NeoPixel(1, ONBOARD_NEO_PIN, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel currentPixel = Adafruit_NeoPixel();
-int led_voltage = HIGH;
+// Global Constants.
+#define NUM_STRIP_LED 60
+
+// Devices.
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_STRIP_LED, PIN_NEO_STRIP, NEO_GRB + NEO_KHZ800);
+ClickEncoder *knob;
+
+// Globals
+int16_t previousEncoderValue, currentEncoderValue;
+uint8_t settingMode, settingValue = 0;
+uint8_t brightness, maximumBrightness = 255;
+uint8_t minimumBrightness = 0;
+uint16_t speed, maximumSpeed = 24;
+uint16_t minimumSpeed = 2;
 
 void setup() {
-    // Onboard NeoPixel.
-    onboard.begin();
-    onboard.show();
-    // Attached NeoPixels.
+    // Start broadcasting on the serial port for debugging.
+    Serial.begin(9600);
+    // Activate our onboard LED.
+    pinMode(PIN_ONBD_LED, OUTPUT);
+    // Activate the NeoPixels.
     strip.begin();
     strip.show();
-    // Onboard LED.
-    pinMode(ONBOARD_LED_PIN, OUTPUT);
+    // Activate the encoder knob.
+    knob = new ClickEncoder(PIN_KNOB_A, PIN_KNOB_B, PIN_KNOB_SWITCH, 4, false);
+    Timer1.initialize(1000);
+    Timer1.attachInterrupt(timerISR);
+    previousEncoderValue = 0;
+    // General things.
+    printSetting();
+    Serial.println("Starting program..");
 }
 
 void loop() {
-    onboardNeoPixel();
-    attachedNeoPixels();
-    onboardLED();
-}
-
-void onboardNeoPixel() {
-    currentPixel = onboard;
-    rainbowCycle(0);
-}
-
-void attachedNeoPixels() {
-    currentPixel = strip;
-    rainbowCycle(20);
-    rainbow(20);
-    theaterChaseRainbow(50);
-}
-
-void onboardLED() {
-    digitalWrite(ONBOARD_LED_PIN, led_voltage);
-    if (led_voltage == HIGH) {
-        led_voltage = LOW;
-    } else {
-        led_voltage = HIGH;
-    }
-}
-
-// Fill the dots one after the other with a color.
-void colorWipe(uint32_t c, uint8_t wait) {
-    for(uint16_t i = 0; i < currentPixel.numPixels(); i++) {
-        currentPixel.setPixelColor(i, c);
-        currentPixel.show();
-        delay(wait);
-    }
-}
-
-void rainbow(uint8_t wait) {
-    uint16_t i, j;
-    for(j = 0; j < 256; j++) {
-        for(i = 0; i < currentPixel.numPixels(); i++) {
-            currentPixel.setPixelColor(i, Wheel((i + j) & 255));
-        }
-        currentPixel.show();
-        delay(wait);
-    }
-}
-
-// Slightly different, this makes the rainbow equally distributed throughout.
-void rainbowCycle(uint8_t wait) {
-    uint16_t i, j;
-    for(j = 0; j < 256 * 5; j++) { // 5 cycles of all colors on wheel.
-        for(i = 0; i < currentPixel.numPixels(); i++) {
-            currentPixel.setPixelColor(i, Wheel(((i * 256 / currentPixel.numPixels()) + j) & 255));
-        }
-        currentPixel.show();
-        delay(wait);
-    }
-}
-
-// Theatre-style crawling lights.
-void theaterChase(uint32_t c, uint8_t wait) {
-    for (int j = 0; j < 10; j++) { //do 10 cycles of chasing.
-        for (int q = 0; q < 3; q++) {
-            for (uint16_t i=0; i < currentPixel.numPixels(); i = i + 3) {
-                currentPixel.setPixelColor(i + q, c); //turn every third currentPixel on.
-            }
-            currentPixel.show();
-            delay(wait);
-            for (uint16_t i = 0; i < currentPixel.numPixels(); i = i + 3) {
-                currentPixel.setPixelColor(i + q, 0); //turn every third currentPixel off.
-            }
-        }
-    }
-}
-
-// Theatre-style crawling lights with rainbow effect.
-void theaterChaseRainbow(uint8_t wait) {
-    for (int j = 0; j < 256; j++) { // cycle all 256 colors in the wheel.
-        for (int q = 0; q < 3; q++) {
-            for (uint16_t i = 0; i < currentPixel.numPixels(); i = i + 3) {
-                currentPixel.setPixelColor(i + q, Wheel((i + j) % 255)); //turn every third currentPixel on.
-            }
-            currentPixel.show();
-            delay(wait);
-            for (uint16_t i = 0; i < currentPixel.numPixels(); i = i + 3) {
-                currentPixel.setPixelColor(i + q, 0); //turn every third currentPixel off.
-            }
-        }
-    }
-}
-
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos) {
-    WheelPos = 255 - WheelPos;
-    if(WheelPos < 85) {
-        return currentPixel.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-    }
-    if(WheelPos < 170) {
-        WheelPos -= 85;
-        return currentPixel.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-    }
-    WheelPos -= 170;
-    return currentPixel.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+    onboardLED(true);
+    readKnob();
 }
